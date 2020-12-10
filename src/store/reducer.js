4f2462aec42aa70f405537b6bdf7592821eeeacc
billyott <willrichott@gmail.com>
2020-12-10 17:37:51 -0500

    create store files

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './store/actions';
+
+const initialState = {
+    testState1: "test1",
+    testState2: "test2"
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
