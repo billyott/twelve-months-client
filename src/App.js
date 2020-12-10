73b23286f234c5cca3ab1bd53abb217b786784ed
billyott <willrichott@gmail.com>
2020-12-10 17:56:13 -0500

    add browser router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 function App() {
 
     return (
-    <div className="App">
-    </div>
+        <div className="App">
+            <BrowserRouter>
+                <Switch >
+                <Route path="/profile">
+                    {!!this.state.jwt ? <Profile user={this.state.user} jwt={this.state.jwt} updateUserHandler={this.updateUserHandler}/> : <Redirect to="/login" />}
+                </Route>
+                <Route path="/categories">
+                    {!!this.state.jwt ? <MainContainer user={this.state.user} jwt={this.state.jwt}/> : <Redirect to="/login" />}
+                </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
     );
 }
 
