52b0637bb4730d9d2c09f39c6aa833e2dc0cf1e8
billyott <willrichott@gmail.com>
2020-12-15 17:36:06 -0500

    update graph style

--- src/components/line-graph.js
+++ src/components/line-graph.js
@@ -1,37 +1,31 @@
-// install (please make sure versions match peerDependencies)
-// yarn add @nivo/core @nivo/line
-import { ResponsiveLine } from '@nivo/line'
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
+import { ResponsiveLine } from '@nivo/line';
+
 import './line-graph.scss';
 
-const LineGraph = ({ data = [] /* see data tab */ }) => (
+const LineGraph = ({ data = []}) => (
     <ResponsiveLine
         data={data}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        margin={{ top: 50, right: 100, bottom: 150, left: 100 }}
         xScale={{ type: 'point' }}
-        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+        yScale={{ type: 'linear', min: 0, max: 11, stacked: false, reverse: false }}
         yFormat=" >-.2f"
         axisTop={null}
         axisRight={null}
         axisBottom={{
             orient: 'bottom',
             tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'transportation',
-            legendOffset: 36,
+            tickPadding: 10,
+            tickRotation: -50,
+            legend: '',
+            legendOffset: 75,
             legendPosition: 'middle'
         }}
         axisLeft={{
             orient: 'left',
             tickSize: 5,
-            tickPadding: 5,
+            tickPadding: 10,
             tickRotation: 0,
-            legend: 'count',
+            legend: 'rating',
             legendOffset: -40,
             legendPosition: 'middle'
         }}
@@ -43,12 +37,12 @@ const LineGraph = ({ data = [] /* see data tab */ }) => (
         useMesh={true}
         legends={[
             {
-                anchor: 'bottom-right',
-                direction: 'column',
+                anchor: 'bottom',
+                direction: 'row',
                 justify: false,
-                translateX: 100,
-                translateY: 0,
-                itemsSpacing: 0,
+                translateX: 0,
+                translateY: 100,
+                itemsSpacing: 20,
                 itemDirection: 'left-to-right',
                 itemWidth: 80,
                 itemHeight: 20,
