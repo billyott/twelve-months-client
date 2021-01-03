2a0ee101ece9c50f853fb7233f03be8dd194fd9c
Billy Ott <willrichott@gmail.com>
2021-01-03 16:10:02 -0500

    add loader to homepage container

--- src/containers/homepage-container.js
+++ src/containers/homepage-container.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux';
+import { Loader } from 'semantic-ui-react';
 
 import DayDetailsCard from '../components/day-details-card';
 import DayCard from '../components/day-card';
@@ -24,14 +25,18 @@ class HomepageContainer extends React.Component {
             <div className="homepage-container">
                 <div className="homepage-container__top">
                     <div className="homepage-container__header">TODAY</div>
-                    <DayDetailsCard day={this.state.days[3]} /> 
+                    {this.state.days.length > 0 ? <DayDetailsCard day={this.state.days[3]} /> : <Loader active inline='centered' />}
                 </div>
                 <div className="homepage-container__bottom">
                     <div className="homepage-container__header">LAST THREE DAYS</div>
                     <NavLink to="/days" className="homepage-container__link">see all days</NavLink>
+                    {this.state.days.length > 0 ? 
                     <ul className="homepage-container__day-cards-list">
                         {this.state.days.slice(0,3).map(day => <li className="homepage-container__day-card-list-item" key={day.id}><DayCard day={day}/></li>)}
                     </ul>
+                    : 
+                    <Loader active inline='centered' />
+                    }
                 </div>
             </div>
         );
